c27f23e57962159290afeb73fa668fb4e2dd3367
GLoGitH <Gert.Longin@ventigrate.be>
2018-04-26 13:21:25 +0200

    initial commit

---
+++ ContossoUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContossoUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
