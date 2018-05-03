0e4584674e700ff58c0fce5fe2ab521e37d453d5
GLoGitH <Gert.Longin@ventigrate.be>
2018-05-03 14:15:42 +0200

    About page -- use model & display just calclated stats from db ....

--- ContossoUniversity/Controllers/HomeController.cs
+++ ContossoUniversity/Controllers/HomeController.cs
@@ -5,22 +5,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContossoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+
 
 namespace ContossoUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+		private readonly SchoolContext _context;
+
+		public HomeController(SchoolContext context)
+		{
+			_context = context;
+		}
+
+		//public IActionResult About()
+		//{
+		//	ViewData["Message"] = "Your application description page.";
+
+		//	return View();
+		//}
+
+		public async Task<ActionResult> About()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+				from student in _context.Students
+				group student by student.EnrollmentDate into dateGroup
+				select new EnrollmentDateGroup()
+				{
+					EnrollmentDate = dateGroup.Key,
+					StudentCount = dateGroup.Count()
+				};
+			return View(await data.AsNoTracking().ToListAsync());
+		}
+
+
+
+		public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
 
         public IActionResult Contact()
         {
